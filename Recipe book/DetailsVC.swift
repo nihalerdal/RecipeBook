52894b001cf7180cee78670ba937291117ba4020
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2020-10-08 16:23:21 -0700

    first version

---
+++ Recipe book/DetailsVC.swift
@@ -0,0 +1,52 @@
+//
+//  DetailsVC.swift
+//  Recipe book
+//
+//  Created by Ruslan Ismayilov on 10/7/20.
+//
+
+import UIKit
+import CoreData
+
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Recognizers
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        self.view.addGestureRecognizer(gestureRecognizer)
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        self.view.addGestureRecognizer(imageTapRecognizer)
+        
+        
+    }
+    @objc func hideKeyboard(){
+        self.view.endEditing(true)
+    }
+
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        print("test")
+    }
+    
+
+}
--- Recipe book/ViewController.swift
+++ Recipe book/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
     }
-
-
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
 }
 
