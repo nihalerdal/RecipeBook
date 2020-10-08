52a37c7c4010624c75c0e3cf7609c478a7d5e856
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2020-10-07 19:10:44 -0700

    Initial Commit

---
+++ Recipe book/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe book
+//
+//  Created by Ruslan Ismayilov on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
